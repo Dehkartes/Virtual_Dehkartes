96e9a2c6939af884cc4fd801409930833f62c648
Dehkartes <hsj3925@naver.com>
2024-11-08 06:44:44 +0900

    feat: Local LLM

---
+++ client.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import requests
+
+FASTAPI_URL = "http://hsj3925.iptime.org:8000/query"
+
+st.title("Bot")
+
+if "messages" not in st.session_state:
+	st.session_state.messages = []
+
+for m in st.session_state["messages"]:
+	with st.chat_message(m["role"]):
+		st.markdown(m["content"])
+		
+if user_msg := st.chat_input("What is up?"):
+	with st.chat_message("user"):
+		st.markdown(user_msg)
+		user_prompt = user_msg
+
+	st.session_state.messages.append({"role": "user", "content": user_msg})
+
+	with st.chat_message("assistant"):
+		# FastAPI 서버에 요청 보내기
+		response = requests.post(FASTAPI_URL, json={"query": user_prompt})
+		if response.status_code == 200:
+			result = response.json().get("answer", "응답을 가져오는 데 실패했습니다.")
+		else:
+			result = "FastAPI 서버 요청이 실패했습니다."
+
+		st.markdown(result)
+		
+	st.session_state.messages.append({
+		"role": "assistant", 
+		"content": result
+	})
